556cf77e32cb97cc33603decab57e13848a0f0bf
napoleon211092 <naphaluan211092@gmail.com>
2023-06-28 13:09:50 +0000

    update

--- get-google-scholar-data.py
+++ get-google-scholar-data.py
@@ -1,6 +1,7 @@
 from scholarly import scholarly
 import json
 from confluent_kafka import Producer, KafkaException
+import paho.mqtt.client as mqtt
 #------------------------------------------FUNCTION------------------------------------------
 def write_json(path: str,json_str: str):
     with open(path, "w") as outfile:
@@ -22,27 +23,56 @@ def send_to_kafka_topic(data):
         print("Message sent successfully.")
     except KafkaException as e:
         print("Failed to send message to Kafka: {}".format(e))
-    # finally:
-    #     producer.close()
+def publish_dict_to_mqtt(dict_data, topic):
+    # Convert the dictionary to a JSON string
+    json_payload = json.dumps(dict_data)
+
+    # Connect to the MQTT broker
+    client = mqtt.Client()
+    client.connect("192.168.107.137", 1883)  # Replace with your MQTT broker address
+
+    # Publish the JSON payload to the specified topic
+    client.publish(topic, payload=json_payload)
+
+    # Disconnect from the MQTT broker
+    client.disconnect()
+def getPublication(author_name):
+    #------------------------------------------FUNCTION------------------------------------------
+    # Retrieve the author's data, fill-in, and print
+    # Get an iterator for the author results
+    search_query = scholarly.search_author(author_name)
+    # Retrieve the first result from the iterator
+    first_author_result = next(search_query)
+    #scholarly.pprint(first_author_result)
+    # Retrieve all the details for the author
+    author = scholarly.fill(first_author_result )
+    #scholarly.pprint(author)
+    publications = author['publications']
+    publications_dict = {}
+    for i in range(len(publications)):
+        publications_dict[i]=publications[i]
+    return publications_dict
+def getAuthorInformation(author_id):
+    author_info = scholarly.search_author_id(author_id)
+    return author_info
 #------------------------------------------FUNCTION------------------------------------------
-# Retrieve the author's data, fill-in, and print
-# Get an iterator for the author results
-author_name = "Lam Tran Thanh Nguyen"
-search_query = scholarly.search_author(author_name)
-# Retrieve the first result from the iterator
-first_author_result = next(search_query)
-#scholarly.pprint(first_author_result)
-
-# Retrieve all the details for the author
-author = scholarly.fill(first_author_result )
-#scholarly.pprint(author)
-
-publications = author['publications']
-publications_dict = {}
-for i in range(len(publications)):
-    print("--------------------------Loop i ="+str(i)+"--------------------------")
-    print(publications[i])
-    publications_dict[i]=publications[i]
-filename = author_name.replace(" ", "-")
-write_json("publication-"+filename+".json",json.dumps(publications_dict))
-send_to_kafka_topic(publications_dict)
+# author_name = "Lam Tran Thanh Nguyen"
+# search_query = scholarly.search_author(author_name)
+# first_author_result = next(search_query)
+# author = scholarly.fill(first_author_result )
+# publications = author['publications']
+# publications_dict = {}
+# for i in range(len(publications)):
+#     print("--------------------------Loop i ="+str(i)+"--------------------------")
+#     print(publications[i])
+#     publications_dict[i]=publications[i]
+# filename = author_name.replace(" ", "-")
+# write_json("publication-"+filename+".json",json.dumps(publications_dict))
+# #Send to kafka
+# #send_to_kafka_topic(publications_dict)
+# #send to MQTT
+# publish_dict_to_mqtt(publications_dict, "publication")
+id = "Yt6EcJYAAAAJ"
+author_name = str(getAuthorInformation(id)["name"])
+publication = getPublication(author_name)
+print(publication)
--- test-1.py
+++ test-1.py
@@ -1,3 +1,6 @@
 from scholarly import scholarly
+import json
 author = scholarly.search_author_id('Yt6EcJYAAAAJ')
-scholarly.pprint(author)
+#scholarly.pprint(author)
+json_payload = json.dumps(author)
+print(json_payload)
