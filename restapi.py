346c8a62656fbf251dceea3c16ec82d10843eb3e
napoleon211092 <naphaluan211092@gmail.com>
2023-06-28 19:30:09 +0000

    update

--- restapi.py
+++ restapi.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 from scholarly import scholarly
 import json
+#----------------------------FUNCTION
+def getAuthor(author_id):
+    author_info = scholarly.search_author_id(author_id)
+    return author_info
+#----------------------------FUNCTION
 app = Flask(__name__)
 @app.route('/')
 def hello():
@@ -11,9 +16,21 @@ def getAuthorInformation():
         id = request.args.get('id')
         author_info = scholarly.search_author_id(id)
         return author_info
-@app.route('/api/publication', methods=['GET'])
-def getPublication():
+@app.route('/api/publicationAuthorName', methods=['GET'])
+def getPublicationByName():
     author_name = request.args.get('name')
+    search_query = scholarly.search_author(author_name)
+    first_author_result = next(search_query)
+    author = scholarly.fill(first_author_result )
+    publications = author['publications']
+    publications_dict = {}
+    for i in range(len(publications)):
+        publications_dict[i]=publications[i]
+    return publications_dict
+@app.route('/api/publicationAuthorId', methods=['GET'])
+def getPublicationById():
+    author_id = request.args.get('id')
+    author_name = str(getAuthor(author_id)["name"])
     search_query = scholarly.search_author(author_name)
     first_author_result = next(search_query)
     author = scholarly.fill(first_author_result )
